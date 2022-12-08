1460467609ef6382d4b46cf92174c38e16c25927
Dave Hobbs <dave_hobbs@uk.ibm.com>
2022-12-08 10:18:29 +0000

    Add the option to delete releases

--- src/rtdelivery/transfer.py
+++ src/rtdelivery/transfer.py
@@ -1,9 +1,11 @@
+import hashlib
 import logging
 import shutil
 from pathlib import Path
 import requests
 import paramiko
 from paramiko import SFTPClient
+from rtdelivery.utils import get_checksum
 
 """ Functions used to enable downloaders and uploaders"""
 
@@ -28,8 +30,23 @@ class FtpUtils:
             sftp.chmod(spath, 0o2770)
             sftp.chdir(spath)
 
-    def ftp_upload(self, src_file: Path, tgt_file: Path):
-        """Upload a file to the server"""
+    def _equal_hashes(
+        self, ssh: paramiko.SSHClient, src_file: Path, tgt_file: Path
+    ) -> bool:
+        """Compare source and target checksums returning true if they are equal.
+        False otherwise, including if the file is missing."""
+        try:
+            _, stdout, _ = ssh.exec_command("sha256sum " + str(tgt_file))
+            tgt_sha256, _, _ = stdout.read().decode().partition(" ")
+            src_sha256 = get_checksum(src_file, hashlib.sha256)
+            compare = tgt_sha256 == src_sha256
+        except IOError:
+            compare = False
+        return compare
+
+    def ftp_upload(self, src_file: Path, tgt_file: Path, overwrite: bool) -> str:
+        """Upload a file to the server. Return a message to say what happened"""
+        msg = ""
         try:
             with paramiko.SSHClient() as ssh:
                 ssh.load_system_host_keys()
@@ -37,11 +54,18 @@ class FtpUtils:
                     self.server, username=self.user_name, password=self.password
                 )
                 with ssh.open_sftp() as sftp:
-                    self._ftp_mkdir(sftp, tgt_file.parent)
-                    sftp.put(str(src_file), str(tgt_file))
-                    sftp.chmod(str(tgt_file), 0o664)
+                    if not overwrite and self._equal_hashes(ssh, src_file, tgt_file):
+                        msg = "Exists"
+                    else:
+                        self._ftp_mkdir(sftp, tgt_file.parent)
+                        sftp.put(str(src_file), str(tgt_file))
+                        sftp.chmod(str(tgt_file), 0o664)
+                        msg = "Uploaded"
+
         except IOError as e:
             logging.exception(f"Failed to upload to {tgt_file}", e)
+            msg = "Upload failed"
+        return msg
 
     def ssh_download(self, src_url: str, tgt_file: Path):
         try:
